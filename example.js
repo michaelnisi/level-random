5a8c2e6a602ae8e93a9473ec4d2bd99a98ff3e26
Michael Nisi <michael.nisi@gmail.com>
2015-01-08 17:17:05 +0100

    Deskill example

--- example.js
+++ example.js
@@ -2,30 +2,14 @@
 var levelup = require('levelup')
 var lr = require('./')
 
-function read (db) {
-  var values = lr({ db: db })
-  values.on('readable', function () {
-    var chunk
-    while (null !== (chunk = values.read())) {
-      console.log('%s', chunk)
-    }
-  })
-  values.on('error', function (er) {
-    console.error(er)
-  })
-  var keys = ['a', 'b']
-  function write () {
-    while (keys.length && values.write(keys.shift())) {}
-    keys.length ? values.on('drain', write) : values.end('c')
-  }
-  write()
-}
-
 levelup('/tmp/level-random.db', function (er, db) {
   db.batch([
-    { type: 'put', key: 'a', value: 'hey' }
-  , { type: 'put', key: 'b', value: 'you' }
+    { type: 'put', key: 'a', value: 'hey\n' }
+  , { type: 'put', key: 'b', value: 'you\n' }
   ], function (er) {
-    read(db)
+    var values = lr({ db: db })
+    values.pipe(process.stdout)
+    values.write('a')
+    values.end('b')
   })
 })
